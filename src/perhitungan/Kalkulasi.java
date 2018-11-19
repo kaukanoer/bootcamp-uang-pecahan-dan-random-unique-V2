841e8c52b4289a1b8e1586b968086c3d7a564896
Mukhlis Purnomo Aji <35167465+MukhlisAji@users.noreply.github.com>
2018-11-19 22:36:12 +0700

    Update Kalkulasi.java

--- src/perhitungan/Kalkulasi.java
+++ src/perhitungan/Kalkulasi.java
@@ -6,18 +6,26 @@ import java.util.Scanner;
 
 public class Kalkulasi {
     
-    static void randomAngka(int panjangData) {
-        ArrayList<Integer> angka = new ArrayList<>();
+    static String randomAngka(int panjangData) {
+        ArrayList<Integer> angka = new ArrayList<>(); 
+        ArrayList<Integer> shuffle = new ArrayList<>();
         for (int element = 0; element <= 9; element++) {
             angka.add(element);
         }
-        Collections.shuffle(angka);
-        int count;
-        for (count = 0; count < panjangData; count++) {
-            System.out.print(angka.get(count));            
+        Collections.shuffle(angka); 
+        for (int count = 0; count < panjangData; count++) {
+            shuffle.add(angka.get(count));
         }
-        System.out.println("");
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = shuffle.size() - 1; i >= 0; i--) {
+          int num = shuffle.get(i);
+          sb.append(num);
+        }
+        String result = sb.toString();
+        return result;
     }
+    
     //Optimalisasi metod untuk menghitung pecahan, menggunakan array
     static void hitungPecahan(int inputUang) {
         int[] uangPecahan = {100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 100, 50};
@@ -35,124 +43,22 @@ public class Kalkulasi {
             }
         }
     }    
-    // metod uang pecahan lama
-//    static void hitungUang(int inputUang) {        
-//        int seratusRibu = 0;
-//        int limaPuluh = 0;
-//        int duaPuluh = 0;
-//        int sepuluh = 0;
-//        int limaRibu = 0;
-//        int duaRibu = 0;
-//        int seribu = 0;
-//        int limaRatus = 0;
-//        int duaRatus = 0;
-//        int seratus = 0;
-//        int limaPuluhPerak = 0;
-//        
-//        while (inputUang > 49) {
-//            if (inputUang >= 100000) {
-//                inputUang -= 100000;
-//                seratusRibu++;
-//            } else if (inputUang >= 50000) {
-//                inputUang -= 50000;
-//                limaPuluh++;
-//            } else if (inputUang >= 20000) {
-//                inputUang -= 20000;
-//                duaPuluh++;
-//            } else if (inputUang >= 10000) {
-//                inputUang -= 10000;
-//                sepuluh++;
-//            } else if (inputUang >= 5000) {
-//                inputUang -= 5000;
-//                limaRibu++;
-//            } else if (inputUang >= 2000) {
-//                inputUang -= 2000;
-//                duaRibu++;
-//            } else if (inputUang >= 1000) {
-//                inputUang -= 1000;
-//                seribu++;
-//            } else if (inputUang >= 500) {
-//                inputUang -= 500;
-//                limaRatus++;
-//            } else if (inputUang >= 200) {
-//                inputUang -= 200;
-//                duaRatus++;
-//            } else if (inputUang >= 100) {
-//                inputUang -= 100;
-//                seratus++;
-//            } else if (inputUang >= 50) {
-//                inputUang -= 50;
-//                limaPuluhPerak++;
-//            } else {
-//                System.out.println("sisa: " + inputUang);
-//            }
-//        }
-//        
-//        System.out.println("Uang Pecahan: ");
-//        if (seratusRibu != 0) {
-//            System.out.println("100.000 " + seratusRibu);
-//        }
-//        if (limaPuluh != 0) {
-//            System.out.println("50.000 \t" + limaPuluh);
-//            
-//        }
-//        if (duaPuluh != 0) {
-//            System.out.println("20.000 \t" + duaPuluh);
-//            
-//        }
-//        if (sepuluh != 0) {
-//            System.out.println("10.000 \t" + sepuluh);
-//            
-//        }
-//        if (limaRibu != 0) {
-//            System.out.println("5.000 \t" + limaRibu);
-//            
-//        }
-//        if (duaRibu != 0) {
-//            System.out.println("2.000 \t" + duaRibu);
-//            
-//        }
-//        if (seribu != 0) {
-//            System.out.println("1.000 \t" + seribu);
-//            
-//        }
-//        if (limaRatus != 0) {
-//            System.out.println("500 \t" + limaRatus);
-//            
-//        }
-//        if (duaRatus != 0) {
-//            System.out.println("200 \t" + duaRatus);
-//            
-//        }
-//        if (seratus != 0) {
-//            System.out.println("100 \t" + seratus);
-//        }
-//        if (limaPuluhPerak != 0) {
-//            System.out.println("50 \t" + limaPuluhPerak);
-//        }
-//        if (inputUang != 0) {
-//            System.out.println("Sisa:" + inputUang);
-//        }
-//        
-//    }
     
     public static void main(String[] args) {
         Kalkulasi panggil = new Kalkulasi();
-//        Boolean ulangi = false;
-//        do {
-//            
-//            int masukAngka = s.nextInt();
-//            if (masukAngka >= 11) {
-//                System.err.println("Angka maksimal adalah 10");
-//                ulangi = true;
-//            } else {
-//                panggil.randomAngka(masukAngka);
-//
-//            }
-//        } while (ulangi = true);
-
-//        System.out.print("Masukkan sejumlah uang: ");
-//        panggil.hitungUang(s.nextInt());
+        Scanner s = new Scanner(System.in);
+        do {
+            System.out.print("Masukkan panjang data: ");
+            int masukAngka = s.nextInt();
+            if (masukAngka >= 11) 
+            {
+                System.err.println("Angka maksimal adalah 10");
+            } else 
+            {
+                System.out.println(panggil.randomAngka(masukAngka));   
+            }
+        } while (true);
+    
         Scanner s = new Scanner(System.in);  
         System.out.print("Masukkan sejumlah uang: ");
         panggil.hitungPecahan(s.nextInt());
